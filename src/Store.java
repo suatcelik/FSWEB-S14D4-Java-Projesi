7ab374a3a812329d1f81ec3dd71c580789721bc4
suat <sc.suatcelik@gmail.com>
2024-06-04 13:30:38 +0300

    proje tamamlandı

--- src/Store.java
+++ src/Store.java
@@ -1,10 +1,20 @@
+import store.models.Bread;
+import store.models.Chocolate;
+import store.models.ProductForSale;
+
 public class Store {
     public static void main(String[] args) {
-
-        listProducts();
+        ProductForSale[] products = new ProductForSale[5];
+        products[0] = new Chocolate("sweet", 15.5,"milky", "white", Boolean.TRUE);
+        products[1] = new Bread("baguette", 8,"wheat", "wheat", "brown");
+        listProducts(products);
     }
 
     public static void listProducts(ProductForSale[] products) {
-
+        for (ProductForSale product : products){
+            if (product != null){
+                product.showDetails();
+            }
+        }
     }
 }
